5b6615d6f117a4d8c71f03dd30638be28c1a15e4
Elena Kolesnikova <elenakolesnikova@Elenas-MacBook-Pro.local>
2021-04-27 21:59:23 +0200

    added media queries

--- src/components/app/app.styled.js
+++ src/components/app/app.styled.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
+import { tablet } from '../../styles/breakpoints';
 
 export const AppStyled = styled.div `
-	margin: 2rem auto 0 auto;
-	max-width: 400px;
+	margin: 2rem .6rem;
+
+	${tablet} {
+		margin: 2rem auto 0 auto;
+		max-width: 400px;
+	}
 `;
